68df33fc46f68413451f936f0388ddf9cf36a39c
Obsidian <stratosphere.programming@gmail.com>
2011-07-14 16:00:51 -0500

    alter the page base for rewriting support down the road

--- includes/Scrii/TF2Stats/Page/Base.php
+++ includes/Scrii/TF2Stats/Page/Base.php
@@ -20,13 +20,29 @@ use \OpenFlame\Framework\Core;
 
 abstract class Base
 {
+	protected $route;
+
 	protected $template_name = '';
-	
+
+	public function setRoute(\OpenFlame\Framework\Router\RouteInstance $route)
+	{
+		$this->route = $route;
+		return $this;
+	}
+
 	public static function newInstance()
 	{
 		return Core::setObject('page.instance', new static());
 	}
-	
+
+	public static function newRoutedInstance(\OpenFlame\Framework\Router\RouteInstance $route)
+	{
+		$self = static::newInstance();
+		$self->setRoute($route);
+
+		return $self;
+	}
+
 	public function getTemplateName()
 	{
 		return $this->template_name;
