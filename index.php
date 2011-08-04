9d395fa8b848cde731884efe610f6a4d12d2454c
Obsidian <stratosphere.programming@gmail.com>
2011-08-03 19:31:14 -0500

    Fix the constants here

--- index.php
+++ index.php
@@ -23,8 +23,7 @@ if(version_compare(PHP_VERSION, '5.3.0', '<'))
 }
 
 // Required constants
-define('Codebite\\Quartz\\SITE_ROOT', dirname(__FILE__));
-define('Scrii\\TF2Stats\\ROOT_PATH', dirname(__FILE__) . '/includes/');
+define('Scrii\\TF2Stats\\ROOT_PATH', dirname(__FILE__));
 define('Scrii\\TF2Stats\\VERSION', '1.0.2');
 
 // Load the bootstrap file
